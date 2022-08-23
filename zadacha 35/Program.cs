9daecdada295081e0cea4645806b44e974548c19
Martin <rauch83@yandex.ru>
2022-08-23 20:21:57 +0300

    samostojatelnaja added

---
+++ zadacha 35/Program.cs
@@ -0,0 +1,54 @@
+﻿// ; *Задача 35:** Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива,
+// ; ;  значения которых лежат в отрезке [10,99].
+void FillArray(int[] collection)   
+{                                    
+                                    
+   
+    int length = collection.Length;  
+    int index = 0;                      
+    while (index < length)           
+    {
+        collection[index] = new Random().Next(1, 1444);
+        //index = index + 1;
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length; 
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position] + " ");
+        position++;
+    }
+Console.WriteLine();
+}
+
+void PrintArray2(int[] col)
+{
+    int count = col.Length; 
+    int position = 0;
+    int a = 0;
+    while(position < count)
+    {
+        if(col[position] < 100)
+        {
+            if(col[position] > 9)
+            {
+                Console.Write(col[position] + " ");
+                a++;            
+            }
+        }        
+        position++;
+    }
+    Console.WriteLine();
+    Console.WriteLine("Количество искомых чисел " + a);
+}
+
+int[] array = new int [123];
+
+FillArray(array);
+PrintArray(array);
+PrintArray2(array);
---
+++ zadacha 37/Program.cs
@@ -0,0 +1,71 @@
+﻿// **Задача 37:** Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+
+// [1 2 3 4 5] -> 5 8 3
+
+// [6 7 3 6] -> 36 21
+
+void FillArray(int[] collection)   
+{                                    
+                                    
+   
+    int length = collection.Length;  
+    int index = 0;                      
+    while (index < length)           
+    {
+        collection[index] = new Random().Next(1, 10);
+        //index = index + 1;
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length; 
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position] + " ");
+        position++;
+    }
+Console.WriteLine();
+}
+
+void Proizvedenije(int[] col)
+{
+    int count = col.Length; 
+    int position = 0;
+    int res = 0;
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    if(count % 2 != 0)
+    {
+        while(a < ((count/2) - 1/2))
+        {
+            res = (col[position + b]) * (col[count - 1 - c]);
+            Console.WriteLine("Ответ " + res);
+            a++;
+            b++;
+            c++;
+        }
+        res = (col[count/2]);
+        Console.WriteLine("Ответ " + res);
+    }
+    else
+    {
+        while(a < (count/2))
+        {
+            res = (col[position + b]) * (col[count - 1 - c]);
+            Console.WriteLine("Ответ " + res);
+            a++;
+            b++;
+            c++;
+        }
+    }
+}
+
+int[] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
+Proizvedenije(array);
